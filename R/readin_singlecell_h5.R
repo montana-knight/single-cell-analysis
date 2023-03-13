a7ae32b5c4c65756a4a300f4bd70dba039753275
Knight <knightm1@reslndbhisbox03.research.chop.edu>
2023-03-13 16:18:38 -0400

    updated file so there would be two functions in one file

--- R/readin_singlecell_h5.R
+++ R/readin_singlecell_h5.R
@@ -13,3 +13,23 @@ readin_singlecell_h5 <- function(filename, sampleid) {
       project = sampleid) -> seu.obj
   return(seu.obj)
 }
+
+#' Import multiple h5 files
+#'
+#' @param filenames The files you want imported, including necessary path information
+#' @param samplenames What you want the samples to be called
+#'
+#' @return A list of seurat objects that correspond to your h5 files
+#' @export
+#'
+
+readin_multiple_h5files <- function(filenames, samplenames){
+  seulist <- list()
+  for (i in 1:length(filenames)) {
+    filename <- filenames[i]
+    sampleid <- samplenames[i]
+    seulist[[i]] <- readin_singlecell_h5(filename, sampleid)
+    names(seulist)[i] <- sampleid
+  }
+  return(seulist)
+}
